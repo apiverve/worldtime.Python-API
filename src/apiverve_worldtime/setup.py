6688df9cdfcc5e41c8a5299392611595b10da3af
APIVerve Team <hello@apiverve.com>
2025-03-28 18:10:00 -0500

    Bug Fixes

--- src/apiverve_worldtime/setup.py
+++ src/apiverve_worldtime/setup.py
@@ -1,14 +1,19 @@
 from setuptools import setup, find_packages
 
+import os
+lib_folder = os.path.dirname(os.path.realpath(__file__))
+requirements_file = os.path.join(lib_folder, 'requirements.txt')
+install_requires = ["requests >= 2.25.1", "setuptools >= 56.0.0"]
+if os.path.exists(requirements_file):
+    with open(requirements_file, 'r') as f:
+        install_requires = f.read().splitlines()
+
 setup(
     name='apiverve_worldtime',
-    version='1.0.9',
+    version='1.1.9',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[
-        'requests',
-        'setuptools'
-    ],
+    install_requires=install_requires,
     description='World Time is a simple tool for getting the current time in any city. It returns the current time, date, and more.',
     author='APIVerve',
     author_email='hello@apiverve.com',
